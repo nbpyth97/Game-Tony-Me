201d4942dc081c0d48c1d71a0eea57f36878ce82
Metalhoerer <70217606+Metalhoerer@users.noreply.github.com>
2020-08-27 15:43:13 +0200

    Update Enemies.py

--- Enemies.py
+++ Enemies.py
@@ -1,23 +1,23 @@
 class Orc:
     def __init__(self, hp, at, de):
-        self.hp = hp
-        self.at = at
-        self.de = de
+        self.hp = 40
+        self.at = 80
+        self.de = 60
 
-orc = Orc(40, 80, 60)
+orc = Orc()
 
 class Croc:
     def __init__(self, hp, at, de):
-        self.hp = hp
-        self.at = at
-        self.de = de
+        self.hp = 80
+        self.at = 60
+        self.de = 40
 
-croc = Croc(80, 60, 40)
+croc = Croc()
 
 class Tuga:
     def __init__(self, hp, at, de):
-        self.hp = hp
-        self.at = at
-        self.de = de
+        self.hp = 60
+        self.at = 40
+        self.de = 80
 
-tuga = Tuga(60, 40, 80)
+tuga = Tuga()
