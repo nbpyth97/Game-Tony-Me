8586fa388f4c745beeac5dc32fb5c38aa798944d
nbpyth97 <69091407+nbpyth97@users.noreply.github.com>
2020-09-06 19:03:46 +0100

    Registration Update
    It takes out the information from the txt file.

--- Registration_and_Login.py
+++ Registration_and_Login.py
@@ -1,21 +1,22 @@
 
 
 def Registration():
-    global nickname, password
+    global nickname, password, lst
     lst = []
     nickname = input('\n Choose your Nickname: ')
     password = input(' Choose your Password: ')
-    password_repeat = input('Repeat your Password: ')
+    password_repeat = input(' Repeat your Password: ')
 
     while password != password_repeat:
-        print('\n Please put the right password =)')
+        print('\n Please put the right password =) ')
         password_repeat = input('\n Repeat your Password: ')
 
     lst.append(nickname)
     lst.append(password_repeat)
 
     with open('E:\\Game\\Registration&Login.txt', 'a') as file:
-        file.write(str(lst))
+        file.write(nickname + '#')
+        file.write(password_repeat)
 
     import Menu
     Menu()
@@ -24,15 +25,24 @@ def Login():
 
     nickname_login = input('Nickname: ')
     password_login = input('Password: ')
-    lst_login = []
 
     with open('E:\\Game\\Registration&Login.txt', 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            lst_login.append(line)
+        lines = file.read().split('#')
 
-    print(lst_login)
+    print(lines[0])
+    while nickname_login != lines[0] or password_login != lines[1]:
+        print('Something wet wrong :/ try again')
+        nickname_login = input('Nickname: ')
+        password_login = input('Password: ')
 
+
+    if nickname_login == lines[0] and password_login == lines[1]:
+        print('''
+        ----(Sub-Menu)----
+           1) Continue the Game
+           2) Restart the Game
+           3) Start the Game
+        ''')
 
 
 
